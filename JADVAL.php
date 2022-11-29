4faeec500a9c0ca8d1f918736c534d3bbdd2d65f
ALI <ebrahimpoordia@gmail.com>
2022-11-29 21:09:25 +0330

    copy from iliya

---
+++ JADVAL.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<table border="6">
+    <?php
+    if (isset($_POST['submit'])) {
+        $r = $_POST['row'];
+        $c = $_POST['col'];
+        $a = 1;
+        $b = 1;
+        while ($a <= $r) {
+            echo "<tr>";
+            while ($b <= $c) {
+                echo "<td>";
+                echo $a * $b;
+                $b++;
+                echo "</td>";
+            }
+            $a++;
+            $b = 1;
+            echo "</tr>";
+        }
+    }
+    ?>
+</table>
+<form method="post">
+    <label>rows:</label> <br/>
+    <input type="number" name="row"/><br/>
+    <label>columns:</label> <br/>
+    <input type="number" name="col"/><br/>
+    <input type="submit" name="submit" value="run"/>
+</form>
+</body>
+</html>
+
+
